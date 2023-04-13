a8db76c232a94c2fa7b1088f2382c506dba25e08
LucasMiranda <lucaswespras@gmail.com>
2023-04-12 22:31:16 -0300

    Inicio

---
+++ src/main/java/com/primeirotrampodev/VendaDeCarros/VendaDeCarrosApplication.java
@@ -0,0 +1,13 @@
+package com.primeirotrampodev.VendaDeCarros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendaDeCarrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendaDeCarrosApplication.class, args);
+	}
+
+}
